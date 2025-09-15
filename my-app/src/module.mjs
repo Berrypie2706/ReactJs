9a3330902158605e97d15eaf1017f0c5bd7f898c
Berrypie2706 <aniket.chouksey444@gmail.com>
2025-09-15 13:41:40 +0530

    props and module

---
+++ my-app/src/module.mjs
@@ -0,0 +1,10 @@
+const a = 'Arpita';
+const b = 'Dev';
+const c = 'butku';
+const d = 'Raj';
+
+export default c;
+export {a};
+export {b};
+export {d};
+export {c};
---
+++ my-app/src/module3.mjs
@@ -0,0 +1,7 @@
+import ex, {a, b, c, d} from './module.mjs'
+
+console.log(ex);
+console.log(a);
+console.log(c);
+console.log(b);
+console.log(d);
